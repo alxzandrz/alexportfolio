8f36103b2a3f1693c5107e267773f627632c7679
alxzandrz <53348840+alxzandrz@users.noreply.github.com>
2023-11-21 01:56:12 +0400

    Update voxel-dog.js

--- components/voxel-dog.js
+++ components/voxel-dog.js
@@ -12,7 +12,8 @@ const VoxelDog = () => {
   const refContainer = useRef()
   const [loading, setLoading] = useState(true)
   const refRenderer = useRef()
-  const urlDogGLB = '/public/dog.glb';
+  const urlDogGLB = (process.env.NODE_ENV === 'production' ? 'https://craftzdog.global.ssl.fastly.net/homepage' : '') + '/dog.glb'
+
 
 
   const handleWindowResize = useCallback(() => {
